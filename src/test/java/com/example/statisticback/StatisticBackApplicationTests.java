98286b9b629f01c2ac9ce5a008aa07c7457c3876
奕-Sugon <yihao168830@163.com>
2022-10-06 22:29:11 +0800

    init static Java project

---
+++ src/test/java/com/example/statisticback/StatisticBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.statisticback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
